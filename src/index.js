fce470a3a44826ffb649cb831de2f3661fe49844
Jay <mantine08@gmail.com>
2021-11-29 22:22:50 -0800

    no message

--- src/index.js
+++ src/index.js
@@ -32,3 +32,11 @@ app.use("/api/", projectsRoute);
 app.listen(PORT, () => {
   console.log("listening on port 3001");
 });
+
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, "../react-ui/build", "index.html");
+  if (!url.startsWith("/app/"))
+    // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
