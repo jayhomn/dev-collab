07eb5c4e97025338b8ed56d53376b3ba8b1f6a08
Jay <mantine08@gmail.com>
2021-11-29 22:12:57 -0800

    no message

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -33,13 +33,31 @@ export default function App() {
     >
       <Router history={history}>
         <Switch>
-          <PrivateRoute path="/createproject" component={CreateProject} />
-          <PrivateRoute path="/myprojects" component={MyProjects} />
-          <PrivateRoute path="/profile" component={Profile} />
-          <PrivateRoute path="/project/:projectId" component={Project} />
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <PrivateRoute path="/newuser" component={NewUser} />
-          <Route path="/" exact>
+          <PrivateRoute
+            path={`${process.env.PUBLIC_URL}/createproject`}
+            component={CreateProject}
+          />
+          <PrivateRoute
+            path={`${process.env.PUBLIC_URL}/myprojects`}
+            component={MyProjects}
+          />
+          <PrivateRoute
+            path={`${process.env.PUBLIC_URL}/profile`}
+            component={Profile}
+          />
+          <PrivateRoute
+            path={`${process.env.PUBLIC_URL}/project/:projectId`}
+            component={Project}
+          />
+          <PrivateRoute
+            path={`${process.env.PUBLIC_URL}/dashboard`}
+            component={Dashboard}
+          />
+          <PrivateRoute
+            path={`${process.env.PUBLIC_URL}/newuser`}
+            component={NewUser}
+          />
+          <Route path={`${process.env.PUBLIC_URL}/`} exact>
             <FrontPage />
           </Route>
         </Switch>
