83e341dd6a0d7accbb981967cd0ae1f2e1974a3e
Jay <mantine08@gmail.com>
2021-11-29 23:16:31 -0800

    no message

--- src/index.js
+++ src/index.js
@@ -29,6 +29,21 @@ app.use(morgan("combined"));
 app.use("/api/", usersRoute);
 app.use("/api/", projectsRoute);
 
+// List of all the files that should be served as-is
+let protected = ["transformed.js", "main.css", "favicon.ico"];
+
+app.get("*", (req, res) => {
+  let path = req.params["0"].substring(1);
+
+  if (protected.includes(path)) {
+    // Return the actual file
+    res.sendFile(`${__dirname}/react-ui/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/react-ui/build/index.html`);
+  }
+});
+
 // starting the server
 app.listen(PORT, () => {
   console.log("listening on port 3001");
